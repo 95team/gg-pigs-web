e553731cf627a50eb1704bc1d12110f2086619c0
kkwongs <kihyun5741@gmail.com>
2020-05-22 18:15:22 +0900

    #3 Prettier를 사용하여 코딩 스타일 통일하기

--- pages/index.vue
+++ pages/index.vue
@@ -11,9 +11,8 @@
         </v-card-title>
         <v-card-text>
           <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
+            Vuetify is a progressive Material Design component framework for Vue.js. It was designed
+            to empower developers to create amazing applications.
           </p>
           <p>
             For more information on Vuetify, check out the
@@ -21,9 +20,7 @@
           </p>
           <p>
             If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
-              discord </a
-            >.
+            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat"> discord </a>.
           </p>
           <p>
             Find a bug? Report it on the github
@@ -36,8 +33,8 @@
             >.
           </p>
           <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
+            Thank you for developing with Vuetify and I look forward to bringing more exciting
+            features in the future.
           </p>
           <div class="text-xs-right">
             <em><small>&mdash; John Leider</small></em>
@@ -63,13 +60,13 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Logo from '~/components/Logo.vue';
+import VuetifyLogo from '~/components/VuetifyLogo.vue';
 
 export default {
   components: {
     Logo,
-    VuetifyLogo
-  }
-}
+    VuetifyLogo,
+  },
+};
 </script>

6588484c9c0e290db2c4781d070231afc70a5c09
LEEHYUNJAE <hjjae2@gmail.com>
2020-06-24 21:12:12 +0900

    #7 Footer layout 개발(default.vue, footerLayout.vue 수정)

--- layouts/footer.vue
+++ layouts/footer.vue
@@ -1,15 +1,15 @@
 <template>
-  <v-footer :fixed="fixed" class="mb2-5 background-white">
-    <v-col class="text-center" cols="12">
-      <hr />
+  <v-footer :fixed="isFooterFixed" class="pa-0 mb-11 background-white">
+    <v-col class="text-center" cols="12" style="padding-left:88px; padding-right:88px">
+      <v-divider />
     </v-col>
     <v-col class="text-right" cols="12">
-      <span class="greyish mr1">
+      <span class="greyish mr-8">
         &copy; {{ new Date().getFullYear() }} PangGo, Inc. All rights reserved
       </span>
-      <span class="greyish mr1">개인정보 처리방침</span>
-      <span class="greyish mr1">이용 약관</span>
-      <span class="greyish mr1">사이트맵</span>
+      <span class="greyish mr-8">개인정보 처리방침</span>
+      <span class="greyish mr-8">이용 약관</span>
+      <span class="greyish" style="margin-right:88px">사이트맵</span>
     </v-col>
   </v-footer>
 </template>
@@ -18,7 +18,7 @@
 export default {
   data() {
     return {
-      fixed: false,
+      isFooterFixed: false,
     };
   },
 };

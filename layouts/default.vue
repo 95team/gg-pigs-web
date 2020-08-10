cc0f6f1a992a10e5438649ba30d92e8db4575252
hjjae2 <hjjae2@gmail.com>
2020-08-10 21:20:04 +0900

    #19 [개발] Default.vue ESLint 오류 개선(콤마), 사이드 NavBar 제거 (이전 커밋은 무시해도 됩니다.)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,52 +1,28 @@
 <template>
-  <div>
-    <v-app-bar class="white" height="116px" flat>
-      <div class="pangoLogo" style="margin-left:88px;">
-        <v-img :src="pangoLogo" width="80px" height="56px"></v-img>
-      </div>
-      <v-spacer></v-spacer>
-      <div class="clickArea mr-8">
-        <img :src="nightDay" class="iconArea" />
-      </div>
-      <div class="clickArea" style="margin-right:88px;">
-        <img :src="userInfo" class="iconArea" />
-      </div>
-    </v-app-bar>
-  </div>
+  <v-app dark>
+    <!-- Header -->
+    <header-layout />
+
+    <!-- Content -->
+    <v-content>
+      <content-layout />
+    </v-content>
+
+    <!-- Footer -->
+    <footer-layout />
+  </v-app>
 </template>
 
 <script>
+import HeaderLayout from '~/layouts/header.vue';
+import FooterLayout from '~/layouts/footer.vue';
+import ContentLayout from '~/layouts/content.vue';
+
 export default {
-  data() {
-    return {
-      pangoLogo: require('../static/img/pangoLogo.png'),
-      nightDay: require('../static/icon/nightDay.svg'),
-      userInfo: require('../static/icon/userInfo.svg'),
-    };
+  components: {
+    HeaderLayout,
+    FooterLayout,
+    ContentLayout,
   },
 };
 </script>
-
-<style scoped>
-.clickArea {
-  width: 48px;
-  height: 48px;
-  position: relative;
-}
-.iconArea {
-  width: 36px;
-  height: 36px;
-  margin: auto;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-}
-</style>
-
-<style>
-.v-toolbar__content {
-  padding: 0;
-}
-</style>

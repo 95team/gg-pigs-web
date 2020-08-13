d669740f80b8c38cedf71015561b050dc7d2df37
Lee.hyunjae <35790290+hjjae2@users.noreply.github.com>
2020-08-13 13:15:37 +0900

    [개발] Default.vue ESLint 오류 개선(콤마), 사이드 NavBar 제거 (#20)
    [개발] Default.vue ESLint 오류 개선(콤마), 사이드 NavBar 제거 (#20)
    
    * #19 [개발] Default.vue ESLint 오류 개선(콤마), 사이드 NavBar 제거
    * #19 [개발] Default.vue ESLint 오류 개선(콤마), 사이드 NavBar 제거 (이전 커밋은 무시해도 됩니다.)
    
    [주요 작업]
    1. Default.vue의 Navigation Bar 제거
    2. ESLint 에 맞춘 콤마(,) 수정

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,5 @@
 <template>
   <v-app dark>
-    <!-- Navigation -->
-    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
-      <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
     <!-- Header -->
     <header-layout />
 
@@ -21,20 +7,6 @@
     <v-content>
       <content-layout />
     </v-content>
-
-    <!-- Navigation -->
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
 
     <!-- Footer -->
     <footer-layout />
@@ -50,30 +22,7 @@ export default {
   components: {
     HeaderLayout,
     FooterLayout,
-    ContentLayout
-  },
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
-    };
+    ContentLayout,
   },
 };
 </script>

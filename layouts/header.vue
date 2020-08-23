1e9c0965e8cfd5c91c48f0dcc1715445b9f87b44
kkwongs <kihyun5741@gmail.com>
2020-08-23 23:57:30 +0900

    #22 headerLayout 개발 (v-toolbar__content 제거)

--- layouts/header.vue
+++ layouts/header.vue
@@ -1,27 +1,23 @@
 <template>
-  <v-flex py-lg-9 py-md-9>
-    <v-app-bar flat height="48px" class="white">
-      <v-container fluid pa-0>
-        <v-layout align-center>
-          <v-flex ml-lg-11 pl-lg-11 ml-md-8 ml-sm-4>
-            <div class="pangoLogo">
-              <v-img :src="pangoLogo"></v-img>
-            </div>
-          </v-flex>
-          <v-flex d-flex justify-end mr-lg-11 pr-lg-10 mr-md-7 mr-sm-1>
-            <v-flex d-flex justify-end mr-lg-8 mr-md-4>
-              <div class="clickArea">
-                <v-img :src="nightDay" class="iconArea"></v-img>
-              </div>
-            </v-flex>
-            <div class="clickArea">
-              <v-img :src="userInfo" class="iconArea"></v-img>
-            </div>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-app-bar>
-  </v-flex>
+  <v-app-bar flat dense class="white">
+    <v-container fluid pa-0>
+      <v-layout align-center>
+        <v-flex>
+          <div class="pangoLogo">
+            <v-img :src="pangoLogo"></v-img>
+          </div>
+        </v-flex>
+        <v-flex d-flex justify-end>
+          <div class="clickArea nightDayMargin">
+            <v-img :src="nightDay" class="iconArea"></v-img>
+          </div>
+          <div class="clickArea userInfoMargin">
+            <v-img :src="userInfo" class="iconArea"></v-img>
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app-bar>
 </template>
 
 <script>
@@ -37,15 +33,16 @@ export default {
 </script>
 
 <style scoped>
+.v-app-bar {
+  height: 116px !important;
+  padding-top: 26px;
+}
+
 .pangoLogo {
   width: 80px;
   height: 56px;
 }
-.clickArea {
-  width: 48px;
-  height: 48px;
-  position: relative;
-}
+
 .iconArea {
   width: 36px;
   height: 36px;
@@ -54,10 +51,52 @@ export default {
   left: 12.5%;
   margin: auto;
 }
+
+.clickArea {
+  width: 48px;
+  height: 48px;
+  position: relative;
+}
+
+@media all and (min-width: 1264px) {
+  .container {
+    max-width: 1880px !important;
+  }
+  .pangoLogo {
+    margin-left: 72px;
+  }
+  .nightDayMargin {
+    margin-right: 32px;
+  }
+  .userInfoMargin {
+    margin-right: 66px;
+  }
+}
+
+@media all and (min-width: 960px) and (max-width: 1263px) {
+  .container {
+    max-width: 932px !important;
+  }
+  .pangoLogo {
+    margin-left: 32px;
+  }
+  .nightDayMargin {
+    margin-right: 16px;
+  }
+  .userInfoMargin {
+    margin-right: 26px;
+  }
+}
+
 @media all and (max-width: 959px) {
+  .v-app-bar {
+    height: 48px !important;
+    padding-top: 0px;
+  }
   .pangoLogo {
     width: 38px;
     height: 26px;
+    margin-left: 14px;
   }
   .iconArea {
     width: 24px;
@@ -67,26 +106,8 @@ export default {
     left: 25%;
     margin: auto;
   }
-}
-@media all and (max-width: 599px) {
-  .container {
-    max-width: 568px !important;
+  .userInfoMargin {
+    margin-right: 2px;
   }
-}
-@media all and (min-width: 600px) and(max-width: 1263px) {
-  .container {
-    max-width: 932px !important;
-  }
-}
-@media all and (min-width: 1264px) {
-  .container {
-    max-width: 1880px !important;
-  }
-}
-</style>
-
-<style>
-.v-toolbar__content {
-  padding: 0;
 }
 </style>

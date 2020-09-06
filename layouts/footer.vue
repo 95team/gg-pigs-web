7450303346fb87b3f373fc59952c7c648c95d85f
kkwongs <kihyun5741@gmail.com>
2020-09-07 00:58:19 +0900

    #29 footerLayout 수정(because of style.css)

--- layouts/footer.vue
+++ layouts/footer.vue
@@ -4,31 +4,21 @@
     <!-- layout1 -->
     <v-layout class="divider">
       <v-flex>
-        <v-divider v-if="dayMode" class="border-white-two" />
-        <v-divider v-else class="border-greyish-brown" />
+        <v-divider :class="[dayMode ? 'border-white-two' : 'border-greyish-brown']" />
       </v-flex>
     </v-layout>
 
     <!-- layout2 -->
     <v-layout class="content">
-      <!-- dayMode -->
-      <v-flex v-if="dayMode">
-        <span class="text greyish">
+      <v-flex>
+        <span class="text" :class="[dayMode ? 'text-greyish' : ' text-warm-grey']">
           &copy; {{ new Date().getFullYear() }} PangGo, Inc. All rights reserved
         </span>
-        <span class="text greyish">개인정보 처리방침</span>
-        <span class="text greyish">이용 약관</span>
-        <span class="greyish">사이트맵</span>
-      </v-flex>
-
-      <!-- nightMode -->
-      <v-flex v-else>
-        <span class="text warm-grey">
-          &copy; {{ new Date().getFullYear() }} PangGo, Inc. All rights reserved
-        </span>
-        <span class="text warm-grey">개인정보 처리방침</span>
-        <span class="text warm-grey">이용 약관</span>
-        <span class="warm-grey">사이트맵</span>
+        <span class="text" :class="[dayMode ? 'text-greyish' : ' text-warm-grey']"
+          >개인정보 처리방침</span
+        >
+        <span class="text" :class="[dayMode ? 'text-greyish' : ' text-warm-grey']">이용 약관</span>
+        <span :class="[dayMode ? 'text-greyish' : ' text-warm-grey']">사이트맵</span>
       </v-flex>
     </v-layout>
   </v-container>

16586002ab6ef73e708cd2e39f91c4cfebab52a0
hjjae2 <hjjae2@gmail.com>
2020-11-01 17:23:15 +0900

    #34 content 수정

--- layouts/content.vue
+++ layouts/content.vue
@@ -5,68 +5,32 @@
       <v-layout class="contents" row wrap>
         <!-- flex1 -->
         <v-flex xs4 md4 lg2>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest2"></ad-box>
-          <ad-box :advert="boxObjectForTest3"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
+          <ad-box v-for="advert in advertisementsFirstColumn" :key="advert.id" :advert="advert" />
         </v-flex>
 
         <!-- flex2 -->
         <v-flex xs4 md4 lg2>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest2"></ad-box>
-          <ad-box :advert="boxObjectForTest3"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
+          <ad-box v-for="advert in advertisementsSecondColumn" :key="advert.id" :advert="advert" />
         </v-flex>
 
         <!-- flex3 -->
         <v-flex xs4 md4 lg2>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest2"></ad-box>
-          <ad-box :advert="boxObjectForTest3"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
+          <ad-box v-for="advert in advertisementsThirdColumn" :key="advert.id" :advert="advert" />
         </v-flex>
 
         <!-- flex4 -->
         <v-flex xs4 md4 lg2>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest2"></ad-box>
-          <ad-box :advert="boxObjectForTest3"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
+          <ad-box v-for="advert in advertisementsFourthColumn" :key="advert.id" :advert="advert" />
         </v-flex>
 
         <!-- flex5 -->
         <v-flex xs4 md4 lg2>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest2"></ad-box>
-          <ad-box :advert="boxObjectForTest3"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
+          <ad-box v-for="advert in advertisementsFifthColumn" :key="advert.id" :advert="advert" />
         </v-flex>
 
         <!-- flex6 -->
         <v-flex xs4 md4 lg2>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest2"></ad-box>
-          <ad-box :advert="boxObjectForTest3"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
-          <ad-box :advert="boxObjectForTest1"></ad-box>
+          <ad-box v-for="advert in advertisementsSixthColumn" :key="advert.id" :advert="advert" />
         </v-flex>
       </v-layout>
       <v-layout class="pages centerAlign">
@@ -93,6 +57,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import AdBox from '~/components/AdBox.vue';
 
 export default {
@@ -102,28 +67,119 @@ export default {
   },
   data() {
     return {
-      boxObjectForTest1: {
-        id: '1',
-        type: '1',
-        title: "This is a adBox's title",
-        alt: "This is a adBox's description",
-        src: 'image/type1.png',
-      },
-      boxObjectForTest2: {
-        id: '1',
-        type: '2',
-        title: "This is a adBox's title",
-        alt: "This is a adBox's description",
-        src: 'image/type2.png',
-      },
-      boxObjectForTest3: {
-        id: '1',
-        type: '3',
-        title: "This is a adBox's title",
-        alt: "This is a adBox's description",
-        src: 'image/type3.png',
-      },
+      advertLayoutSize: 6,
+      advertisementsFirstColumn: [],
+      advertisementsSecondColumn: [],
+      advertisementsThirdColumn: [],
+      advertisementsFourthColumn: [],
+      advertisementsFifthColumn: [],
+      advertisementsSixthColumn: [],
     };
+  },
+  computed: {
+    ...mapGetters({
+      advertisements: 'advertisements/fetchAdvertisements',
+    }),
+  },
+  watch: {
+    advertisements() {
+      this.advertisementsFirstColumn = this.makeFilledAdvertsTargetColumn('1');
+      this.advertisementsSecondColumn = this.makeFilledAdvertsTargetColumn('2');
+      this.advertisementsThirdColumn = this.makeFilledAdvertsTargetColumn('3');
+      this.advertisementsFourthColumn = this.makeFilledAdvertsTargetColumn('4');
+      this.advertisementsFifthColumn = this.makeFilledAdvertsTargetColumn('5');
+      this.advertisementsSixthColumn = this.makeFilledAdvertsTargetColumn('6');
+    },
+  },
+  created() {
+    const vm = this;
+    vm.init();
+  },
+  methods: {
+    init() {
+      const vm = this;
+      vm.$store.dispatch('advertisements/FETCH_LIST');
+    },
+
+    getAdvertsFromColumn(targetColumn) {
+      const filteredAdverts = this.advertisements.filter(
+        advert => advert.columnPosition === targetColumn,
+      );
+      filteredAdverts.sort((advert1, advert2) =>
+        advert1.rowPosition > advert2.rowPosition ? 1 : -1,
+      );
+
+      return filteredAdverts;
+    },
+
+    getAdvertsLocations(filteredAdverts) {
+      const EMPTY = 0;
+      const FILLED = 1;
+      const advertsLocations = Array.from({ length: this.advertLayoutSize + 2 }, () => EMPTY);
+      advertsLocations[0] = FILLED;
+      advertsLocations[this.advertLayoutSize + 1] = FILLED;
+
+      for (let i = 0; i < filteredAdverts.length; i++) {
+        const advertType = filteredAdverts[i].advertisementType;
+        const advertRowPosition = Number(filteredAdverts[i].rowPosition);
+
+        for (let j = 0; j < Number(advertType.charAt(1)); j++) {
+          advertsLocations[advertRowPosition + j] = FILLED;
+        }
+      }
+
+      return advertsLocations;
+    },
+
+    makeEmptyAdvert(targetRow, targetColumn, emptyAdvertSize) {
+      const randomSeed = 10000;
+      const id = randomSeed + (Date.now() % randomSeed) * Math.floor(Math.random() * randomSeed);
+      targetRow = targetRow.toString();
+      targetColumn = targetColumn.toString();
+      emptyAdvertSize = emptyAdvertSize.toString();
+
+      const emptyAdvert = {
+        id,
+        type: 'EXAMPLE',
+        size: emptyAdvertSize,
+        rowPosition: targetRow,
+        columnPosition: targetColumn,
+      };
+
+      return emptyAdvert;
+    },
+
+    makeFilledAdvertsTargetColumn(targetColumn) {
+      const EMPTY = 0;
+      const FILLED = 1;
+      const filledAdverts = this.getAdvertsFromColumn(targetColumn);
+      const advertsLocations = this.getAdvertsLocations(filledAdverts);
+
+      let emptyAdvertSize = 0;
+      for (let i = 1; i <= this.advertLayoutSize + 1; i++) {
+        if (advertsLocations[i] === EMPTY) {
+          emptyAdvertSize++;
+        }
+
+        if (advertsLocations[i] === FILLED && emptyAdvertSize >= 1) {
+          const targetRow = i - emptyAdvertSize;
+          filledAdverts.push(this.makeEmptyAdvert(targetRow, targetColumn, emptyAdvertSize));
+          emptyAdvertSize = 0;
+        } else if (emptyAdvertSize >= 3) {
+          emptyAdvertSize = 3;
+          const targetRow = i + 1 - emptyAdvertSize;
+          filledAdverts.push(this.makeEmptyAdvert(targetRow, targetColumn, emptyAdvertSize));
+          emptyAdvertSize = 0;
+        }
+        advertsLocations[i] = FILLED;
+      }
+
+      filledAdverts.sort((advert1, advert2) =>
+        advert1.rowPosition > advert2.rowPosition ? 1 : -1,
+      );
+
+      return filledAdverts;
+    },
   },
 };
 </script>

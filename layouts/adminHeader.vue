5dfdbb0d88172923ad0efa48251695abbfc1dadf
kkwongs <kihyun5741@gmail.com>
2020-11-06 18:05:51 +0900

    #37 modify adminHeader.vue

--- layouts/adminHeader.vue
+++ layouts/adminHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <v-container fluid pa-0>
     <div style="margin-left: 2%;">
       <img src="../static/image/pangoLogoDay.png" alt="광고돼지" width="118px" height="39px" />
     </div>
@@ -8,7 +8,7 @@
       <div class="appLink" style="float:left; margin-right: 19px;">노션</div>
       <div class="appLink" style="float:right;">슬랙</div>
     </div>
-  </header>
+  </v-container>
 </template>
 
 <script>
@@ -16,7 +16,7 @@ export default {};
 </script>
 
 <style scoped>
-header {
+.container {
   max-width: 1920px;
   height: 130px;
   border-bottom: solid 1px #707070;

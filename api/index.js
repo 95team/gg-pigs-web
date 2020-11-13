88ed4af56e55016b691b094816988c6448434f1f
Lee.hyunjae <35790290+hjjae2@users.noreply.github.com>
2020-11-13 14:23:25 +0900

    [개발] pango API 연동 테스트(with Axios) & 메인 페이지 광고 적용 (#36)
    [개발] pango API 연동 테스트(with Axios) & 메인 페이지 광고 적용 (#36)
    
    - 광고 API 를 사용하여 광고를 노출시킬 수 있도록 하였습니다.
    - 광고 노출 시, 빈 공간에 empty 이미지가 노출될 수 있도록 하였습니다.

---
+++ api/index.js
@@ -0,0 +1,6 @@
+const baseHost = 'http://hj2server.ddns.net';
+const basePort = '8484';
+const basePath = 'api/v1';
+const baseApiUrl = baseHost + ':' + basePort + '/' + basePath;
+
+export { baseHost, basePort, basePath, baseApiUrl };

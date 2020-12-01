46a5cd4a1221111defdeb39c8d6ff7a6675a4589
hjjae2 <hjjae2@gmail.com>
2020-12-01 21:14:09 +0900

    #44 Add config 'timeout'(10sec) to requestVerificationMail

--- api/verificationMail.js
+++ api/verificationMail.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 import { baseApiUrl } from './index.js';
 
 function requestVerificationMail(payload) {
-  return axios.post(`${baseApiUrl}/verification-mails`, payload);
+  return axios.post(`${baseApiUrl}/verification-mails`, payload, {
+    timeout: 10000,
+  });
 }
 
 export { requestVerificationMail };

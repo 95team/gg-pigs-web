c2afaa81e2533174d55ce6ae791edde7868d8e85
hjjae2 <hjjae2@gmail.com>
2020-12-24 21:33:25 +0900

    #57 메소드명, 변수명, 주석 리팩토링

--- pages/index.vue
+++ pages/index.vue
@@ -94,12 +94,12 @@ export default {
   },
   watch: {
     posters() {
-      this.postersFirstColumn = this.makeFilledPostersTargetColumn('1');
-      this.postersSecondColumn = this.makeFilledPostersTargetColumn('2');
-      this.postersThirdColumn = this.makeFilledPostersTargetColumn('3');
-      this.postersFourthColumn = this.makeFilledPostersTargetColumn('4');
-      this.postersFifthColumn = this.makeFilledPostersTargetColumn('5');
-      this.postersSixthColumn = this.makeFilledPostersTargetColumn('6');
+      this.postersFirstColumn = this.makeFullPosters('1');
+      this.postersSecondColumn = this.makeFullPosters('2');
+      this.postersThirdColumn = this.makeFullPosters('3');
+      this.postersFourthColumn = this.makeFullPosters('4');
+      this.postersFifthColumn = this.makeFullPosters('5');
+      this.postersSixthColumn = this.makeFullPosters('6');
     },
   },
   created() {
@@ -114,6 +114,7 @@ export default {
 
     getPostersFromColumn(targetColumn) {
       const filteredPosters = this.posters.filter(poster => poster.columnPosition === targetColumn);
+
       filteredPosters.sort((poster1, poster2) =>
         poster1.rowPosition > poster2.rowPosition ? 1 : -1,
       );
@@ -122,22 +123,65 @@ export default {
     },
 
     getPostersLocations(filteredPosters) {
+      /**
+       * 한 '열'에 대해서, 어떤 '행'에 '포스터'가 존재하는지 배열 생성하는 함수입니다. (존재: 1, 미존재: 0)
+       * 양쪽의 끝 인덱스(0, layout + 1)는 사용하지 않습니다.
+       * Ex1. 모든 행이 비었을 때 : [1, 0, 0, 0, 0, 0, 0, 1]
+       * Ex2. 1, 2 행이 존재할 때 : [1, 1, 1, 0, 0, 0, 0, 1]
+       */
+
       const EMPTY = 0;
       const FILLED = 1;
-      const postersLocations = Array.from({ length: this.posterLayoutSize + 2 }, () => EMPTY);
-      postersLocations[0] = FILLED;
-      postersLocations[this.posterLayoutSize + 1] = FILLED;
+      const locations = Array.from({ length: this.posterLayoutSize + 2 }, () => EMPTY);
+
+      locations[0] = FILLED;
+      locations[this.posterLayoutSize + 1] = FILLED;
 
       for (let i = 0; i < filteredPosters.length; i++) {
         const posterType = filteredPosters[i].posterType;
         const posterRowPosition = Number(filteredPosters[i].rowPosition);
 
         for (let j = 0; j < Number(posterType.charAt(1)); j++) {
-          postersLocations[posterRowPosition + j] = FILLED;
+          locations[posterRowPosition + j] = FILLED;
         }
       }
 
-      return postersLocations;
+      return locations;
+    },
+
+    makeFullPosters(targetColumn) {
+      /**
+       * 한 '열'에 대해서, ('포스터'가 없는) 빈 '행'의 위치에 '샘플 포스터'를 삽입하여 모든 '행'을 채우는 함수입니다.
+       */
+      const EMPTY = 0;
+      const FILLED = 1;
+      const posters = this.getPostersFromColumn(targetColumn);
+      const postersLocations = this.getPostersLocations(posters);
+
+      const fullPosters = posters;
+
+      let emptyPosterSize = 0;
+      for (let i = 1; i <= this.posterLayoutSize + 1; i++) {
+        if (postersLocations[i] === EMPTY) {
+          emptyPosterSize++;
+        }
+
+        if (postersLocations[i] === FILLED && emptyPosterSize >= 1) {
+          const targetRow = i - emptyPosterSize;
+          fullPosters.push(this.makeEmptyPoster(targetRow, targetColumn, emptyPosterSize));
+          emptyPosterSize = 0;
+        } else if (emptyPosterSize >= 3) {
+          emptyPosterSize = 3;
+          const targetRow = i + 1 - emptyPosterSize;
+          fullPosters.push(this.makeEmptyPoster(targetRow, targetColumn, emptyPosterSize));
+          emptyPosterSize = 0;
+        }
+        postersLocations[i] = FILLED;
+      }
+
+      fullPosters.sort((poster1, poster2) => (poster1.rowPosition > poster2.rowPosition ? 1 : -1));
+
+      return fullPosters;
     },
 
     makeEmptyPoster(targetRow, targetColumn, emptyPosterSize) {
@@ -158,38 +202,6 @@ export default {
       return emptyPoster;
     },
 
-    makeFilledPostersTargetColumn(targetColumn) {
-      // 한 '열'에 대해서, ('포스터'가 없는) 빈 '행'의 위치에 '샘플 포스터'를 삽입하여 모든 '행'을 채우는 함수
-      const EMPTY = 0;
-      const FILLED = 1;
-      const filledPosters = this.getPostersFromColumn(targetColumn);
-      const postersLocations = this.getPostersLocations(filledPosters);
-
-      let emptyPosterSize = 0;
-      for (let i = 1; i <= this.posterLayoutSize + 1; i++) {
-        if (postersLocations[i] === EMPTY) {
-          emptyPosterSize++;
-        }
-
-        if (postersLocations[i] === FILLED && emptyPosterSize >= 1) {
-          const targetRow = i - emptyPosterSize;
-          filledPosters.push(this.makeEmptyPoster(targetRow, targetColumn, emptyPosterSize));
-          emptyPosterSize = 0;
-        } else if (emptyPosterSize >= 3) {
-          emptyPosterSize = 3;
-          const targetRow = i + 1 - emptyPosterSize;
-          filledPosters.push(this.makeEmptyPoster(targetRow, targetColumn, emptyPosterSize));
-          emptyPosterSize = 0;
-        }
-        postersLocations[i] = FILLED;
-      }
-
-      filledPosters.sort((poster1, poster2) =>
-        poster1.rowPosition > poster2.rowPosition ? 1 : -1,
-      );
-
-      return filledPosters;
-    },
     nightDayUpdate() {
       if (this.dayMode) {
         this.dayMode = false;

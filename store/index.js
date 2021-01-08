c1e17a4a61f489627f1c22118b6c3ab3569391a6
hjjae2 <hjjae2@gmail.com>
2021-01-08 14:29:33 +0900

    #80 vuex for login-user

--- store/index.js
+++ store/index.js
@@ -1,11 +1,24 @@
 export const state = function() {
-  return { user: null };
+  return {
+    isLogin: false,
+    loginUser: null,
+  };
 };
 
 export const mutations = {
-  SET_USER(state, payload) {
-    state.user = payload;
+  LOGIN_SUCCESS(state, payload) {
+    state.isLogin = payload.isLogin;
+    state.loginUser = payload.loginUser;
   },
 };
 
 export const actions = {};
+
+export const getters = {
+  isLogin(state) {
+    return state.isLogin;
+  },
+  loginUser(state) {
+    return state.loginUser;
+  },
+};

a819c5b4e3e3530399a414f6c85cd200f779b181
lee.hyunjae <35790290+hjjae2@users.noreply.github.com>
2021-02-04 23:34:21 +0900

    [개발] 빈 포스터 이미지 변경 (EMPTY -> +) (#129)
    [개발] 빈 포스터 이미지 변경 (EMPTY -> +) (#129)
    
    1. 빈 포스터에 노출되는 이미지 변경 (EMPTY -> +)
    -  빈 포스터는 무조건 1x1 크기 적용
    - opacity 0.7 적용

--- components/PosterBox.vue
+++ components/PosterBox.vue
@@ -69,48 +69,18 @@ export default {
   data() {
     return {
       posterBox: {},
-      emptyPosterType1: {
+      emptyPoster: {
         id: 0,
         title: '광고를 신청해주세요!',
         description: '광고를 신청해주세요!',
         keywords: '광고를 신청해주세요!',
-        imagePath: 'image/empty_type1.png',
-        siteUrl: 'www.example.com',
+        imagePath: 'image/empty-poster.png',
+        siteUrl: '/',
         rowPosition: '-',
         columnPosition: '-',
         posterType: ['R1', 'example-image'],
         posterWidth: '300',
         posterHeight: '250',
-        startedDate: '2020-01-01',
-        finishedDate: '2030-01-01',
-      },
-      emptyPosterType2: {
-        id: 0,
-        title: '광고를 신청해주세요!',
-        description: '광고를 신청해주세요!',
-        keywords: '광고를 신청해주세요!',
-        imagePath: 'image/empty_type2.png',
-        siteUrl: 'www.example.com',
-        rowPosition: '-',
-        columnPosition: '-',
-        posterType: ['R2', 'example-image'],
-        posterWidth: '300',
-        posterHeight: '516',
-        startedDate: '2020-01-01',
-        finishedDate: '2030-01-01',
-      },
-      emptyPosterType3: {
-        id: 0,
-        title: '광고를 신청해주세요!',
-        description: '광고를 신청해주세요!',
-        keywords: '광고를 신청해주세요!',
-        imagePath: 'image/empty_type3.png',
-        siteUrl: 'www.example.com',
-        rowPosition: '-',
-        columnPosition: '-',
-        posterType: ['R3', 'example-image'],
-        posterWidth: '300',
-        posterHeight: '782',
         startedDate: '2020-01-01',
         finishedDate: '2030-01-01',
       },
@@ -133,14 +103,7 @@ export default {
   methods: {
     init() {
       if (this.poster.type !== undefined && this.poster.type === 'EXAMPLE') {
-        this.posterBox = this.emptyPosterType1;
-        if (this.poster.size === '1') {
-          this.posterBox = this.emptyPosterType1;
-        } else if (this.poster.size === '2') {
-          this.posterBox = this.emptyPosterType2;
-        } else if (this.poster.size === '3') {
-          this.posterBox = this.emptyPosterType3;
-        }
+        this.posterBox = this.emptyPoster;
       } else {
         this.posterBox = this.poster;
       }
@@ -155,7 +118,7 @@ img {
 }
 
 .example-image {
-  opacity: 0.1;
+  opacity: 0.7;
 }
 
 .center-content {

aa416d85b0de408b4151a2f4a189eea4b5e975ca
kkwongs <kihyun5741@gmail.com>
2021-05-11 14:21:25 +0900

    #143 reanme dashboard icons & add menu icons, owner store

---
+++ store/owner.js
@@ -0,0 +1,55 @@
+export const state = function() {
+  return {
+    /** '로그인 사용자' 관련 변수입니다. */
+    isLogin: false,
+    loginUser: null,
+
+    /** '이미지' 관련 변수입니다. */
+    dashboardDay: require('~/static/icon/dashboardDay.svg'),
+    dashboardNight: require('~/static/icon/dashboardNight.svg'),
+    menuDay: require('~/static/icon/menuDay.svg'),
+    menuNight: require('~/static/icon/menuNight.svg'),
+  };
+};
+
+export const mutations = {
+  /** '로그인 사용자' 관련 mutation 입니다. */
+  LOGIN_SUCCESS(state, payload) {
+    state.isLogin = payload.isLogin;
+    state.loginUser = payload.loginUser;
+  },
+};
+
+export const actions = {};
+
+export const getters = {
+  /** '로그인 사용자' 관련 getter 입니다. */
+  isLogin(state) {
+    return state.isLogin;
+  },
+  loginUser(state) {
+    return state.loginUser;
+  },
+  ownerPath(state) {
+    const admin = 'ROLE_ADMIN';
+    return state.isLogin && state.loginUser
+      ? state.loginUser.role === admin
+        ? '/admin/lists'
+        : '/owner'
+      : '/owner/login';
+  },
+
+  /** '이미지' 관련 getter 입니다. */
+  dashboardDay(state) {
+    return state.dashboardDay;
+  },
+  dashboardNight(state) {
+    return state.dashboardNight;
+  },
+  menuDay(state) {
+    return state.menuDay;
+  },
+  menuNight(state) {
+    return state.menuNight;
+  },
+};
